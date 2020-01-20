af0bcec7dfb750d0ba11e2aa87d7753d463b9280
Kamal Kumar <kamal7.kumar@ril.com>
2020-01-20 18:57:57 +0530

    spark code removed from hadoop-repo

--- src/main/java/hbase/HBaseReads.java
+++ src/main/java/hbase/HBaseReads.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class HBaseReads {
 
-    // TODO => write & read are important.
+    // TODO :=> write & read are important.
     //  Other concepts/operations can be ignored for now, Okay.
 
     public static void main(String[] args) throws Exception {
--- src/main/java/hbase/HBaseWrites.java
+++ src/main/java/hbase/HBaseWrites.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HBaseWrites {
+
     public static void main(String[] args) throws Exception {
-        // TODO => write & read are important.
+        // TODO :=> write & read are important.
         //  Other concepts/operations can be ignored for now, Okay.
 
         /* Put(rowkey(string)) ,
@@ -21,21 +22,21 @@ public class HBaseWrites {
         Put put = new Put(Bytes.toBytes("1150"),1234567890);
         put.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name"), Bytes.toBytes("KAMAL"));
 
-        //Put put2 = new Put(Bytes.toBytes("116"));
-        //put2.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("EC"), Bytes.toBytes("55019688"));
-
-        /* Get table from Connection() */
+        Put put2 = new Put(Bytes.toBytes("116"));
+        put2.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("EC"), Bytes.toBytes("55019688"));
 
         List<Put> putList = new ArrayList<Put>();
         putList.add(put);
-        //putList.add(put2);
+        putList.add(put2);
 
+        /* Get table from Connection() */
         Table table = HBaseUtils.getTable("emp");
 
 
         /* void put(Put put) throws IOException; */
         /* void put(List<Put> putList) throws IOException; */
         table.put(putList);
+        table.put(put);
         table.close();
 
         System.out.println("data inserted");
